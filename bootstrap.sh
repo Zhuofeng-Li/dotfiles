40cc1b408cae7883a248bc75d2e8b2a928172987
Zhuofeng-Li <1004271927@qq.com>
2026-05-11 11:42:52 -0700

    refactor: move karabiner to subdir, switch bootstrap to GitHub clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-DOTFILES_DIR="$HOME/Library/Mobile Documents/com~apple~CloudDocs/karabiner"
+DOTFILES_DIR="$HOME/dotfiles"
+GITHUB_REPO="https://github.com/Zhuofeng-Li/dotfiles.git"
 
 echo "==> Installing Xcode Command Line Tools..."
 xcode-select --install 2>/dev/null || true
@@ -10,6 +11,13 @@ echo "==> Installing Homebrew..."
 if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+echo "==> Cloning dotfiles from GitHub..."
+if [ -d "$DOTFILES_DIR" ]; then
+  git -C "$DOTFILES_DIR" pull
+else
+  git clone "$GITHUB_REPO" "$DOTFILES_DIR"
 fi
 
 echo "==> Installing packages from Brewfile..."
@@ -25,8 +33,9 @@ mkdir -p ~/.config/fish
 cp "$DOTFILES_DIR/fish/config.fish" ~/.config/fish/config.fish
 
 echo "==> Setting up Karabiner config..."
+mkdir -p ~/.config
 rm -rf ~/.config/karabiner
-ln -sf "$DOTFILES_DIR" ~/.config/karabiner
+ln -sf "$DOTFILES_DIR/karabiner" ~/.config/karabiner
 
 echo "==> Restoring app settings with mackup..."
 cp "$DOTFILES_DIR/mackup.cfg" ~/.mackup.cfg
